5be2930d65b516ae6f3239070c07db897476dbcd
robert-vo <robertvo79@gmail.com>
2014-07-24 12:36:58 -0500

    Update app.js
    Changed urlSanitizationWhitelist to aHrefSanitizationWhitelist

--- js/app.js
+++ js/app.js
@@ -5,5 +5,6 @@ var googlemaps = angular.module('googlemaps', ['adaptive.googlemaps']);
 //this was used to whitelist the 'geo' and 'maps' URIs so that angular will accept them.
 //if the url sanitization is ignored, angularJS will stick 'unsafe:' as a prefix to the geo and maps URIs.
 googlemaps.config(function($compileProvider){
-      $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|geo|maps):/);
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|geo|maps):/);
+      //For older versions of AngularJS, use urlSanitizationWhitelist instead of aHrefSanitizationWhitelist
     });
